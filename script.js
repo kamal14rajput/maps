82d1de5bb3bb5d44610b4ab20a2f8070f561a1e4
kamal14rajput <kamal14rajput@gmail.com>
2021-10-14 14:16:29 +0530

    Map declaration

---
+++ script.js
@@ -0,0 +1,6 @@
+const question = new Map();
+question.set('question','What is the official name of the latest Javascript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
