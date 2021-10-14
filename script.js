c913db247fa89ea2244f3d197659a254aea4656c
kamal14rajput <kamal14rajput@gmail.com>
2021-10-14 14:50:21 +0530

    question and answers

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ question.set(1, 'ES5');
 question.set(2, 'ES6');
 question.set(3, 'ES2015');
 question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct Answer!!!');
+question.set(false, 'Wrong Answer!Try again');
+
+console.log(question.get('question'));
+//console.log(question.size);
+
+/*
+question.delete(4);
+if(question.has(3)){
+    question.delete(2);
+}
+question.clear();
+*/
+//question.forEach((key, value) => console.log(`This is ${key},and it's set to ${value} `));
+
+/*
+for(let [key, value] of question.entries()){
+    console.log(`This is ${key},and it's set to ${value} `);
+}
+*/
+for(let [key, value] of question.entries()){
+    if(typeof(key) === 'number'){
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
